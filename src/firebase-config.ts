5a7dce7c1f69813b37e773436ba468d46f61e0b2
florescente <florescente@gmail.com>
2022-07-31 07:48:45 -0300

    enviromental variables

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'todo-7b7a4.firebaseapp.com',
-  projectId: 'todo-7b7a4',
-  storageBucket: 'todo-7b7a4.appspot.com',
-  messagingSenderId: '929946635850',
-  appId: '1:929946635850:web:8cfa17f3ad54ca59cda0b3',
-  measurementId: 'G-SLFNMFELFN',
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSANGER_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 }
 
 const app = initializeApp(firebaseConfig)
